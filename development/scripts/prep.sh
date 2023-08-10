f791b234c47ed22182619d2f7d4a20c3ec698d3a
Nic O'Connor <noconnor@cert.org>
2023-08-10 15:18:37 -0400

    bug: prep script had wrong paths

--- development/scripts/prep.sh
+++ development/scripts/prep.sh
@@ -4,6 +4,7 @@ echo "Prep Script!"
 echo "Working Directory is: $dir"
 # Create ssl root certificate
 mkdir -p $dir/development/ssl
-openssl genrsa -out $dir/development/ssl/key
-openssl req -x509 -new -nodes -key $dir/development/ssl/cert \
+openssl genrsa -out $dir/development/ssl/tls.key
+openssl req -x509 -new -nodes -key $dir/development/ssl/tls.key \
     -days 3650 -sha256 -out $dir/development/ssl/tls.crt -subj "/CN=crucible-ca"
+cp $dir/development/ssl/tls.crt $dir/development/ssl/root-ca.pem
