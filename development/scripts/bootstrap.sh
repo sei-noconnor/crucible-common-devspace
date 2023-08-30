690ea6ddafa66890a03c04e76fd12e9a08fe9047
Nic O'Connor <noconnor@cert.org>
2023-08-30 13:35:20 -0400

    update keycloak quarkus for admin endpoint

--- development/scripts/bootstrap.sh
+++ development/scripts/bootstrap.sh
@@ -3,5 +3,5 @@ dir=$1
 kind_bin=$(which kind)
 kubectl_bin=$(which kubectl)
 envsubst < $dir/development/bootstrap-kind-config.yaml | $kind_bin create cluster --config - 
-$kubectl_bin apply -f $dir/development/deps/ingress-nginx//nginx-ingress.yaml
+
 
