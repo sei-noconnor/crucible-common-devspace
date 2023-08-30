45b20c8f89d16aa614bc49d0b754a5e9fc4c677a
Nic O'Connor <noconnor@cert.org>
2023-08-30 16:17:30 -0400

    remove scripts in favor of in-line functions

--- development/scripts/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dir=$1
-kind_bin=$(which kind)
-kubectl_bin=$(which kubectl)
-envsubst < $dir/development/bootstrap-kind-config.yaml | $kind_bin create cluster --config - 
-
-
--- development/scripts/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Clean Script"
-dir=$1
-kind_bin=$(which kind)
-kubectl_bin=$(which kubectl)
-$kind_bin delete cluster -n $(yq '.name' < $dir/development/bootstrap-kind-config.yaml)
-# Cleanup Certs
-# echo "Deleteing SSL Certificates"
-# rm -rf $dir/development/ssl
